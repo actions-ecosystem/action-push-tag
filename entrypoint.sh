d660e47d9464b8027c032bd519038fdd598df89e
Jeff Blayney <jeffrey.blayney@missionlane.com>
2023-01-12 10:46:40 -0700

    adding inputs for force and ref

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,21 @@ set -e
 
 tag=${INPUT_TAG}
 message=${INPUT_MESSAGE}
+ref=${INPUT_REF}
+force=${INPUT_FORCE}
+
+if [ "$force" = "true" ]; then
+  force="-f"
+else
+  unset force
+fi
+
+if [ -z "$ref" ]; then
+  ref="HEAD"
+fi
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-git tag -a "${tag}" -m "${message}"
-git push origin "${tag}"
+git tag -a "${tag}" "${ref}" -m "${message}" ${force}
+git push origin "${tag}" "$force"
